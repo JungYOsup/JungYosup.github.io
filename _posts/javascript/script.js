8b436fd0facd4614a72189107790cec4311759d3
yosup <yosup1004@gmail.com>
2020-12-23 12:26:06 +0900

    modify

--- _posts/javascript/script.js
+++ _posts/javascript/script.js
@@ -1,77 +1,14 @@
-// network로 요청을해서 백엔드에서 데이터를 가져오는 함수가 있다고 하자.
+var a = 20;
 
-// const { func } = require("prop-types");
-
-// function fetchUser() {
-//   //do network request in 10 secs....
-
-//   return new Promise((resolve, reject) => {
-//     resolve("ellie");
-//   });
-// }
-
-// const user = fetchUser();
-// console.log(user);
-// user.then(console.log);
-
-// async function fetchUser() {
-//   //do network request in 10 secs....
-//   return "ellie";
-// }
-
-// const user = fetchUser();
-// console.log(user);
-// user.then(console.log);
-
-// 2. await
-
-// function delay(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
-
-// async function getApple() {
-//   await delay(3000);
-//   return "사과";
-// }
-
-// function getBanana() {
-//   return delay(3000).then(() => "바나나");
-// }
-
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+function abc() {
+  console.log(a);
+  var b = 30;
 }
 
-// function getApple() {
-//   return delay(1000).then(() => "사과");
-// }
-
-// function getBanana() {
-//   return delay(1000).then(() => "바나나");
-// }
-
-// function pickFruits() {
-//   return getApple().then((apple) => {
-//     return getBanana().then((banana) => `${apple} + ${banana}`);
-//   });
-// }
-
-// pickFruits().then(console.log);
-
-async function getApple() {
-  await delay(1000);
-  return "사과";
+{
+  var c = 40;
 }
 
-async function getBanana() {
-  await delay(1000);
-  return "바나나";
-}
-
-function pickAllFruits() {
-  return Promise.all([getApple(), getBanana()]).then((fruits) =>
-    fruits.join("+")
-  );
-}
-
-pickAllFruits().then(console.log);
+abc(); // 20
+console.log(c); // 40
+console.log(b); // b is not defined
